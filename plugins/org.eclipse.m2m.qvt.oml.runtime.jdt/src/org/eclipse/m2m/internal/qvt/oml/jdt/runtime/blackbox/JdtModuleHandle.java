7e3eca4ffad9988ee852a96b379df57253933b8a
Christopher Gerking <christopher.gerking@kit.edu>
2022-05-16 21:35:55 +0200

    [579914] Consider dynamic @Module annotations

--- plugins/org.eclipse.m2m.qvt.oml.runtime.jdt/src/org/eclipse/m2m/internal/qvt/oml/jdt/runtime/blackbox/JdtModuleHandle.java
+++ plugins/org.eclipse.m2m.qvt.oml.runtime.jdt/src/org/eclipse/m2m/internal/qvt/oml/jdt/runtime/blackbox/JdtModuleHandle.java
@@ -10,7 +10,14 @@
  *******************************************************************************/
 package org.eclipse.m2m.internal.qvt.oml.jdt.runtime.blackbox;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.m2m.internal.qvt.oml.QvtPlugin;
 import org.eclipse.m2m.internal.qvt.oml.blackbox.java.ModuleHandle;
+import org.eclipse.m2m.qvt.oml.blackbox.java.Module;
 
 public class JdtModuleHandle extends ModuleHandle {
 	
@@ -24,6 +31,39 @@ public class JdtModuleHandle extends ModuleHandle {
 	@Override
 	public Class<?> getModuleJavaClass() {
 		return moduleJavaClass;
+	}
+	
+	private static Annotation getModuleAnnotation(Class<?> c) throws ClassNotFoundException {
+		Class<?> moduleAnnotationClass = c.getClassLoader().loadClass(Module.class.getCanonicalName());
+				
+		for(Annotation a : c.getAnnotations()) {
+			if (a.annotationType() == moduleAnnotationClass) {
+				return a;
+			}
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public List<String> getUsedPackages() {				
+		try {						
+			Annotation annotation = getModuleAnnotation(getModuleJavaClass());
+			
+			if (annotation != null) {
+				Method packageURIsMethod = annotation.getClass().getMethod("packageURIs"); //$NON-NLS-1$
+				Object packageURIs = packageURIsMethod.invoke(annotation);
+				
+				if (packageURIs instanceof String[]) {
+					return Arrays.asList((String[]) packageURIs);
+				}
+			}
+		}
+		catch(Exception e) {
+			QvtPlugin.error(e);
+		}
+		
+		return super.getUsedPackages();
 	}
 
 }
