5a57dacc695f92c61cc72ce173ecbe6957291ac0
Ed Willink <ed@willink.me.uk>
2023-11-22 09:44:19 +0000

    [releng] refresh copyrights

--- tests/org.eclipse.m2m.tests.qvt.oml/parserTestData/models/bug577992/src/org/eclipse/m2m/tests/qvt/oml/bbox/bug577992/impl/Bug577992FactoryImpl.java
+++ tests/org.eclipse.m2m.tests.qvt.oml/parserTestData/models/bug577992/src/org/eclipse/m2m/tests/qvt/oml/bbox/bug577992/impl/Bug577992FactoryImpl.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
 /*******************************************************************************
- * Copyright (c) 2022 Christopher Gerking and others.
+ * Copyright (c) 2022, 2023 Christopher Gerking and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
