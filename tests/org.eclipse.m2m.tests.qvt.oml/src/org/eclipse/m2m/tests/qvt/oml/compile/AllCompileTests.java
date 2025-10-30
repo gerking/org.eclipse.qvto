4ca338997d871ec1bd8ab915995fc91b0081dbaf
ChristopherGerking <ChristopherGerking@i43pc193.sdq.kastel.kit.edu>
2025-10-30 12:50:08 +0100

    [#1135] Clean up test resources

--- tests/org.eclipse.m2m.tests.qvt.oml/src/org/eclipse/m2m/tests/qvt/oml/compile/AllCompileTests.java
+++ tests/org.eclipse.m2m.tests.qvt.oml/src/org/eclipse/m2m/tests/qvt/oml/compile/AllCompileTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2018 Borland Software Corporation and others.
+ * Copyright (c) 2009, 2025 Borland Software Corporation and others.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
@@ -8,9 +8,13 @@
  *   
  * Contributors:
  *     Borland Software Corporation - initial API and implementation
+ *     Christopher Gerking - bug #1135
  *******************************************************************************/
 package org.eclipse.m2m.tests.qvt.oml.compile;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.m2m.tests.qvt.oml.TestProject;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -18,5 +22,10 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({PlatformPluginUnitResolverTest.class, UnitResolverFactoryTest.class, QVTOCompilerTest.class, URIUnitResolverTest.class})
 public class AllCompileTests {
-	
+		
+	@AfterClass
+	public static void tearDownAfterClass() throws CoreException {
+		TestProject testProject = TestProject.getExistingProject(UnitResolverFactoryTest.DEPLOYED_PROJECT_NAME);		
+		if (testProject != null) testProject.delete();
+	}
 }
